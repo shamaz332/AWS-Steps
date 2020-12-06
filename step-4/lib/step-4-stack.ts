45348c68ca99cf9ac2c454a00532600efaa5c205
shamaz332 <shamazsaeed332@gmail.com>
2020-12-06 22:32:19 +0500

    done

--- step-4/lib/step-4-stack.ts
+++ step-4/lib/step-4-stack.ts
@@ -3,91 +3,85 @@ import * as appsync from '@aws-cdk/aws-appsync';
 import * as ddb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
 
-export class Step4Stack extends cdk.Stack {
+export class AppsyncCdkAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Creates the AppSync API
     const api = new appsync.GraphqlApi(this, 'Api', {
       name: 'cdk-notes-appsync-api',
       schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
       authorizationConfig: {
         defaultAuthorization: {
           authorizationType: appsync.AuthorizationType.API_KEY,
-         
         },
       },
       xrayEnabled: true,
     });
 
+    // print out the AppSync GraphQL endpoint to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+     value: api.graphqlUrl
+    });
 
+    // print out the AppSync API Key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || ''
+    });
 
-  // Prints out the AppSync GraphQL endpoint to the terminal
-  new cdk.CfnOutput(this, "GraphQLAPIURL", {
-    value: api.graphqlUrl
-   });
+    // print out the stack region
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region
+    });
 
-   // Prints out the AppSync GraphQL API key to the terminal
-   new cdk.CfnOutput(this, "GraphQLAPIKey", {
-     value: api.apiKey || ''
-   });
+    const notesLambda = new lambda.Function(this, 'AppSyncNotesHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'main.handler',
+      code: lambda.Code.fromAsset('lambda-fns'),
+      memorySize: 1024
+    });
+    
+    // set the new Lambda function as a data source for the AppSync API
+    const lambdaDs = api.addLambdaDataSource('lambdaDatasource', notesLambda);
 
-   // Prints out the stack region to the terminal
-   new cdk.CfnOutput(this, "Stack Region", {
-     value: this.region
-   });
+    // create resolvers to match GraphQL operations in schema
+    lambdaDs.createResolver({
+      typeName: "Query",
+      fieldName: "getNoteById"
+    });
 
-//adding lambda
+    lambdaDs.createResolver({
+      typeName: "Query",
+      fieldName: "listNotes"
+    });
 
-const notesLambda  = new lambda.Function(this,"Noteshandler",{
-  runtime: lambda.Runtime.NODEJS_12_X,
-  handler: 'todo.handler',
-  code: lambda.Code.fromAsset('functions'),
-  memorySize: 1024
-})
-// Set the new Lambda function as a data source for the AppSync API
-const lambdaDs = api.addLambdaDataSource('lambdaDatasource', notesLambda);
+    lambdaDs.createResolver({
+      typeName: "Mutation",
+      fieldName: "createNote"
+    });
 
+    lambdaDs.createResolver({
+      typeName: "Mutation",
+      fieldName: "deleteNote"
+    });
 
-lambdaDs.createResolver({
-  typeName: "Query",
-  fieldName: "getNoteById"
-});
+    lambdaDs.createResolver({
+      typeName: "Mutation",
+      fieldName: "updateNote"
+    });
 
-lambdaDs.createResolver({
-  typeName: "Query",
-  fieldName: "listNotes"
-});
+    // create DynamoDB table
+    const notesTable = new ddb.Table(this, 'CDKNotesTable', {
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING,
+      },
+    });
 
-lambdaDs.createResolver({
-  typeName: "Mutation",
-  fieldName: "createNote"
-});
-
-lambdaDs.createResolver({
-  typeName: "Mutation",
-  fieldName: "deleteNote"
-});
-
-lambdaDs.createResolver({
-  typeName: "Mutation",
-  fieldName: "updateNote"
-});
-
-//adding dynamodb]
-const notesTable = new ddb.Table(this, 'CDKNotesTable', {
-  billingMode: ddb.BillingMode.PAY_PER_REQUEST,
-  partitionKey: {
-    name: 'id',
-    type: ddb.AttributeType.STRING,
-  },
-});
-// enable the Lambda function to access the DynamoDB table (using IAM)
-notesTable.grantFullAccess(notesLambda)
-
-// Create an environment variable that we will use in the function code
-notesLambda.addEnvironment('NOTES_TABLE', notesTable.tableName);
-
+    // enable the Lambda function to access the DynamoDB table (using IAM)
+    notesTable.grantFullAccess(notesLambda)
+    
+    notesLambda.addEnvironment('NOTES_TABLE', notesTable.tableName);
 
   }
 }
