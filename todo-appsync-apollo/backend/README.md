3ba94cd81936c115267385d3c336e760553b4063
shamaz332 <shamazsaeed332@gmail.com>
2020-12-09 21:59:42 +0500

    13A removed

--- todo-appsync-apollo/backend/README.md
+++
@@ -1,3 +0,0 @@
-This is the todo app
-
-https://aws.amazon.com/blogs/mobile/building-scalable-graphql-apis-on-aws-with-cdk-and-aws-appsync/
--- todo-appsync-apollo/backend/bin/step-4.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { TodoApollo } from '../lib/step-4-stack';
-
-const app = new cdk.App();
-new TodoApollo(app, 'TodoApollo');
